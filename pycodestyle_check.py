f809a958ce8a32c3f4216acc75c51b3749f1220c
EphraimShikanga <ephraimshikanga@gmail.com>
2023-05-13 15:28:02 +0300

    AirBnB_Clone

---
+++ pycodestyle_check.py
@@ -0,0 +1,7 @@
+def my_function(*args, **kwargs):
+    for arg in args:
+        print(arg)
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
+
+my_function("arg1", "arg2", keyword1="value1", keyword2="value2")
